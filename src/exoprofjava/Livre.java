06636bd37a42631c491849e8e0bc753f91265972
AZOUG-Kylian-24020313 <kylian.azoug@etu.univ-amu.fr>
2025-02-04 17:18:09 +0100

    classe livre finie

--- src/exoprofjava/Livre.java
+++ src/exoprofjava/Livre.java
@@ -1,5 +1,30 @@
 package exoprofjava;
 
 public class Livre {
-
+	private String name;
+	private double price;
+	private int quantite;
+	
+	public Livre ( String name, double price, int quantite)  {
+		this.name = name;
+		this.price = price;
+		this.quantite = quantite;
+	}
+	
+	public String getName() {
+		return name;
+		
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public int getQuantite() {
+		return quantite;
+	}
+	
+	public String toString() {
+		return "Le nom de ce livre est : " + name + ", le prix pour ce livre est de " + price + " $ nous avons " + quantite + " examplaire(s) de ce livre ";
+	}
 }
--- src/exoprofjava/Main.java
+++ src/exoprofjava/Main.java
@@ -3,7 +3,8 @@ package exoprofjava;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Livre l1 = new Livre ("onepiece", 10, 1);
+		System.out.println(l1.toString());
 
 	}
 
