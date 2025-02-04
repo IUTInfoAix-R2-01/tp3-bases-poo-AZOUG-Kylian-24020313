dc31ced81df2acb74e253dbe4db4dec177fbd5ad
AZOUG-Kylian-24020313 <kylian.azoug@etu.univ-amu.fr>
2025-02-04 15:07:05 +0100

    Base Java

---
+++ src/exoprofjava/Livre.java
@@ -0,0 +1,5 @@
+package exoprofjava;
+
+public class Livre {
+
+}
---
+++ src/exoprofjava/Main.java
@@ -0,0 +1,10 @@
+package exoprofjava;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
